5657164e49df5a60edb17b3e304afc4607c3ceef
dominicbarmet <103999107+dominicbarmet@users.noreply.github.com>
2022-05-15 22:13:40 +0200

    Add files via upload

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,294 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   IR_KASKADE_MS_FAR_180_EDITED_wav;
+    const int            IR_KASKADE_MS_FAR_180_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_MS_FAR_0_EDITED_wav;
+    const int            IR_KASKADE_MS_FAR_0_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_MS_MED_0_EDITED_wav;
+    const int            IR_KASKADE_MS_MED_0_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_MS_MED_180_EDITED_wav;
+    const int            IR_KASKADE_MS_MED_180_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_MS_NEAR_180_EDITED_wav;
+    const int            IR_KASKADE_MS_NEAR_180_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_MS_NEAR_0_EDITED_wav;
+    const int            IR_KASKADE_MS_NEAR_0_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_XY_FAR_180_EDITED_wav;
+    const int            IR_KASKADE_XY_FAR_180_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_XY_FAR_0_EDITED_wav;
+    const int            IR_KASKADE_XY_FAR_0_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_XY_MED_180_EDITED_wav;
+    const int            IR_KASKADE_XY_MED_180_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_XY_MED_0_EDITED_wav;
+    const int            IR_KASKADE_XY_MED_0_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_XY_NEAR_180alt_EDITED_wav;
+    const int            IR_KASKADE_XY_NEAR_180alt_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_XY_NEAR_180_EDITED_wav;
+    const int            IR_KASKADE_XY_NEAR_180_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_XY_NEAR_0_EDITED_wav;
+    const int            IR_KASKADE_XY_NEAR_0_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_AB_FAR_180alt_EDITED_wav;
+    const int            IR_KASKADE_AB_FAR_180alt_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_AB_FAR_180_EDITED_wav;
+    const int            IR_KASKADE_AB_FAR_180_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_AB_FAR_0_EDITED_wav;
+    const int            IR_KASKADE_AB_FAR_0_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_AB_MED_180_EDITED_wav;
+    const int            IR_KASKADE_AB_MED_180_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_AB_MED_0_EDITED_wav;
+    const int            IR_KASKADE_AB_MED_0_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_AB_NEAR_180_EDITED_wav;
+    const int            IR_KASKADE_AB_NEAR_180_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KASKADE_AB_NEAR_0_EDITED_wav;
+    const int            IR_KASKADE_AB_NEAR_0_EDITED_wavSize = 1151848;
+
+    extern const char*   IR_KS2_MS_FAR_180_EDITED_wav;
+    const int            IR_KS2_MS_FAR_180_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_MS_FAR_0_EDITED_wav;
+    const int            IR_KS2_MS_FAR_0_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_MS_MED_0_EDITED_wav;
+    const int            IR_KS2_MS_MED_0_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_MS_MED_180_EDITED_wav;
+    const int            IR_KS2_MS_MED_180_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_MS_NEAR_180_EDITED_wav;
+    const int            IR_KS2_MS_NEAR_180_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_MS_NEAR_0_EDITED_wav;
+    const int            IR_KS2_MS_NEAR_0_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_XY_FAR_180_EDITED_wav;
+    const int            IR_KS2_XY_FAR_180_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_XY_FAR_0_EDITED_wav;
+    const int            IR_KS2_XY_FAR_0_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_XY_MED_180_EDITED_wav;
+    const int            IR_KS2_XY_MED_180_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_XY_MED_0_EDITED_wav;
+    const int            IR_KS2_XY_MED_0_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_XY_NEAR_180_EDITED_wav;
+    const int            IR_KS2_XY_NEAR_180_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_XY_NEAR_0_EDITED_wav;
+    const int            IR_KS2_XY_NEAR_0_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_AB_FAR_180_EDITED_wav;
+    const int            IR_KS2_AB_FAR_180_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_AB_FAR_0_EDITED_wav;
+    const int            IR_KS2_AB_FAR_0_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_AB_MED_180_EDITED_wav;
+    const int            IR_KS2_AB_MED_180_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_AB_MED_0_EDITED_wav;
+    const int            IR_KS2_AB_MED_0_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_AB_NEAR_180_EDITED_wav;
+    const int            IR_KS2_AB_NEAR_180_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS2_AB_NEAR_0_EDITED_wav;
+    const int            IR_KS2_AB_NEAR_0_EDITED_wavSize = 1152736;
+
+    extern const char*   IR_KS3_MS_FAR_180_EDITED_wav;
+    const int            IR_KS3_MS_FAR_180_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_MS_FAR_0_EDITED_wav;
+    const int            IR_KS3_MS_FAR_0_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_MS_MED_0_EDITED_wav;
+    const int            IR_KS3_MS_MED_0_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_MS_MED_180_EDITED_wav;
+    const int            IR_KS3_MS_MED_180_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_MS_NEAR_180_EDITED_wav;
+    const int            IR_KS3_MS_NEAR_180_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_MS_NEAR_0_EDITED_wav;
+    const int            IR_KS3_MS_NEAR_0_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_AB_FAR_180_EDITED_wav;
+    const int            IR_KS3_AB_FAR_180_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_XY_FAR_180_EDITED_wav;
+    const int            IR_KS3_XY_FAR_180_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_XY_FAR_0_EDITED_wav;
+    const int            IR_KS3_XY_FAR_0_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_AB_FAR_0_EDITED_wav;
+    const int            IR_KS3_AB_FAR_0_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_AB_MED_180_EDITED_wav;
+    const int            IR_KS3_AB_MED_180_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_XY_MED_180_EDITED_wav;
+    const int            IR_KS3_XY_MED_180_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_XY_MED_0_EDITED_wav;
+    const int            IR_KS3_XY_MED_0_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_AB_MED_0_EDITED_wav;
+    const int            IR_KS3_AB_MED_0_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_XY_NEAR_180_EDITED_wav;
+    const int            IR_KS3_XY_NEAR_180_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_XY_NEAR_0_EDITED_wav;
+    const int            IR_KS3_XY_NEAR_0_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_AB_NEAR_180_EDITED_wav;
+    const int            IR_KS3_AB_NEAR_180_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_KS3_AB_NEAR_0_EDITED_wav;
+    const int            IR_KS3_AB_NEAR_0_EDITED_wavSize = 1150120;
+
+    extern const char*   IR_TURM_AB_FAR_0_EDITED_wav;
+    const int            IR_TURM_AB_FAR_0_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_AB_FAR_180_EDITED_wav;
+    const int            IR_TURM_AB_FAR_180_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_AB_MED_0_EDITED_wav;
+    const int            IR_TURM_AB_MED_0_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_AB_MED_180_EDITED_wav;
+    const int            IR_TURM_AB_MED_180_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_AB_NEAR_0_EDITED_wav;
+    const int            IR_TURM_AB_NEAR_0_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_AB_NEAR_180_EDITED_wav;
+    const int            IR_TURM_AB_NEAR_180_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_MS_FAR_0_EDITED_wav;
+    const int            IR_TURM_MS_FAR_0_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_MS_FAR_180_EDITED_wav;
+    const int            IR_TURM_MS_FAR_180_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_MS_MED_0_EDITED_wav;
+    const int            IR_TURM_MS_MED_0_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_MS_MED_180_EDITED_wav;
+    const int            IR_TURM_MS_MED_180_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_MS_NEAR_0_EDITED_wav;
+    const int            IR_TURM_MS_NEAR_0_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_MS_NEAR_180_EDITED_wav;
+    const int            IR_TURM_MS_NEAR_180_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_XY_FAR_0_EDITED_wav;
+    const int            IR_TURM_XY_FAR_0_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_XY_FAR_180_EDITED_wav;
+    const int            IR_TURM_XY_FAR_180_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_XY_MED_0_EDITED_wav;
+    const int            IR_TURM_XY_MED_0_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_XY_MED_180_EDITED_wav;
+    const int            IR_TURM_XY_MED_180_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_XY_NEAR_0_EDITED_wav;
+    const int            IR_TURM_XY_NEAR_0_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_XY_NEAR_0alt_EDITED_wav;
+    const int            IR_TURM_XY_NEAR_0alt_EDITED_wavSize = 1151464;
+
+    extern const char*   IR_TURM_XY_NEAR_180_EDITED_wav;
+    const int            IR_TURM_XY_NEAR_180_EDITED_wavSize = 1151464;
+
+    extern const char*   Background_png;
+    const int            Background_pngSize = 333474;
+
+    extern const char*   ButtonRed_png;
+    const int            ButtonRed_pngSize = 73622;
+
+    extern const char*   ButtonUp_png;
+    const int            ButtonUp_pngSize = 51528;
+
+    extern const char*   ButtonDown_png;
+    const int            ButtonDown_pngSize = 63209;
+
+    extern const char*   Knob_Shadow_png;
+    const int            Knob_Shadow_pngSize = 20027;
+
+    extern const char*   KnobDark_png;
+    const int            KnobDark_pngSize = 574968;
+
+    extern const char*   Knob_png;
+    const int            Knob_pngSize = 689256;
+
+    extern const char*   Knobdot_png;
+    const int            Knobdot_pngSize = 403490;
+
+    extern const char*   Photo_Turm_jpg;
+    const int            Photo_Turm_jpgSize = 1571219;
+
+    extern const char*   Photo_Konzertsaal3_jpg;
+    const int            Photo_Konzertsaal3_jpgSize = 2600720;
+
+    extern const char*   Photo_Konzertsaal2_jpg;
+    const int            Photo_Konzertsaal2_jpgSize = 1183184;
+
+    extern const char*   Photo_Kaskadenhalle_jpg;
+    const int            Photo_Kaskadenhalle_jpgSize = 1750128;
+
+    extern const char*   Photo_Turm_png;
+    const int            Photo_Turm_pngSize = 718186;
+
+    extern const char*   ToniVerbIcon_V2_png;
+    const int            ToniVerbIcon_V2_pngSize = 74812;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 89;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
